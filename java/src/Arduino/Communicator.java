1246bb29772d6015ba2fa51ef29ac41bfd6b94e8
Felipe Duzzi <duzzifelipe@gmail.com>
2016-10-23 20:47:47 -0200

    java events code

--- java/src/Arduino/Communicator.java
+++ java/src/Arduino/Communicator.java
@@ -1,41 +1,106 @@
 package Arduino;
 
-import gnu.io.CommPortIdentifier;
-import gnu.io.NoSuchPortException;
-import gnu.io.SerialPort;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+import java.util.Enumeration;
 
-public class Communicator {
-    private OutputStream serialOut;
-    private String comPort;
-    private int tax;
 
-    public Communicator(String comPort, int tax) {
-        this.comPort = comPort;
-        this.tax = tax;
-        this.initialize();
+public class Communicator implements SerialPortEventListener {
+    SerialPort serialPort;
+    /** The port we're normally going to use. */
+    private static final String PORT_NAMES[] = {
+            "/dev/tty.usbserial-A9007UX1", // Mac OS X
+            "/dev/ttyACM0", // Raspberry Pi
+            "/dev/ttyUSB0", // Linux
+            "COM3", // Windows
+    };
+    /**
+     * A BufferedReader which will be fed by a InputStreamReader
+     * converting the bytes into characters
+     * making the displayed results codepage independent
+     */
+    private BufferedReader input;
+    /** The output stream to the port */
+    private OutputStream output;
+    /** Milliseconds to block while waiting for port open */
+    private static final int TIME_OUT = 2000;
+    /** Default bits per second for COM port. */
+    private static final int DATA_RATE = 9600;
+
+    public void initialize() {
+        // the next line is for Raspberry Pi and
+        // gets us into the while loop and was suggested here was suggested http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
+        System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
+
+        CommPortIdentifier portId = null;
+        Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+
+        //First, Find an instance of serial port as set in PORT_NAMES.
+        while (portEnum.hasMoreElements()) {
+            CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+            for (String portName : PORT_NAMES) {
+                if (currPortId.getName().equals(portName)) {
+                    portId = currPortId;
+                    break;
+                }
+            }
+        }
+        if (portId == null) {
+            System.out.println("Could not find COM port.");
+            return;
+        }
+
+        try {
+            // open serial port, and use class name for the appName.
+            serialPort = (SerialPort) portId.open(this.getClass().getName(),
+                    TIME_OUT);
+
+            // set port parameters
+            serialPort.setSerialPortParams(DATA_RATE,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+
+            // open the streams
+            input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+            output = serialPort.getOutputStream();
+
+            // add event listeners
+            serialPort.addEventListener(this);
+            serialPort.notifyOnDataAvailable(true);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
     }
 
-    private void initialize() {
-        try {
-            CommPortIdentifier portId = null;
-            try {
-                portId = CommPortIdentifier.getPortIdentifier(this.comPort);
-            } catch (NoSuchPortException npe) {
-                // Port not found
-                npe.printStackTrace();
-            }
-
-            SerialPort port = (SerialPort) portId.open("Serial communication", this.tax);
-            serialOut = port.getOutputStream();
-            port.setSerialPortParams(this.tax, //taxa de transferência da porta serial
-                    SerialPort.DATABITS_8, //taxa de 10 bits 8 (envio)
-                    SerialPort.STOPBITS_1, //taxa de 10 bits 1 (recebimento)
-                    SerialPort.PARITY_NONE); //receber e enviar dados
-        } catch (Exception e) {
-            e.printStackTrace();
+    /**
+     * This should be called when you stop using the port.
+     * This will prevent port locking on platforms like Linux.
+     */
+    public synchronized void close() {
+        if (serialPort != null) {
+            serialPort.removeEventListener();
+            serialPort.close();
         }
+    }
+
+    /**
+     * Handle an event on the serial port. Read the data and print it.
+     */
+    public synchronized void serialEvent(SerialPortEvent oEvent) {
+        if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+            try {
+                String inputLine=input.readLine();
+                System.out.println(inputLine);
+            } catch (Exception e) {
+                System.err.println(e.toString());
+            }
+        }
+        // Ignore all the other eventTypes, but you should consider the other ones.
     }
 }
--- java/src/Main.java
+++ java/src/Main.java
@@ -1,6 +1,18 @@
+import Arduino.Communicator;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Communicator main = new Communicator();
+        main.initialize();
+        Thread t = new Thread() {
+            public void run() {
+                //the following line will keep this app alive for 1000 seconds,
+                //waiting for events to occur and responding to them (printing incoming messages to console).
+                try {Thread.sleep(1000000);} catch (InterruptedException ie) {}
+            }
+        };
+        t.start();
+        System.out.println("Started");
     }
 }
